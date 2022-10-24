0ef9bbff46cf0a094a1d1da8dc770dad954a51c3
Larissa Dinart <lari_dinart@hotmail.com>
2022-10-24 18:42:37 -0300

    Adicionar arquivos de projeto.

---
+++ OnTheFly_Final/Controllers/PassengerController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnTheFly_Final.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PassengerController : ControllerBase
+    {
+    }
+}
---
+++ OnTheFly_Final/Models/Passenger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnTheFly_Final.Models
+{
+    public class Passenger
+    {
+        [Required,StringLength(14, ErrorMessage = "CPF inválido!")]
+        public String Cpf { get; set; }
+        [Required(ErrorMessage = "O campo Nome é obrigatório!"), StringLength(30,ErrorMessage = "Nome inválido!")]
+        public String Name { get; set; }
+        [Required(ErrorMessage = "O campo Gênero é obrigatório!")]
+        public char Gender { get; set; }
+        [Required,StringLength(14, ErrorMessage = "Telefone inválido!")]
+        public String Phone { get; set; }
+        [Required(ErrorMessage = "O campo Data de Nascimento é obrigatório!")]
+        public DateTime DtBirth { get; set; }
+        public DateTime DtRegister { get; set; }
+        public bool Status { get; set; }
+    }
+}
