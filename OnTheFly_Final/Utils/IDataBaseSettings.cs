49eaf7b6f3b9c45e99c5d6ecf3a07aa5a6260421
Larissa Dinart <lari_dinart@hotmail.com>
2022-10-25 13:24:57 -0300

    Controllers e Services

---
+++ OnTheFly_Final/Utils/IDataBaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace OnTheFly_Final.Utils
+{
+    public class IDataBaseSettings
+    {
+        public string PassengerCollectionName { get; set; }
+        public string PassengerCollectionGarbage { get; set; }
+        public string PassengerCollectionRestricted { get; set; }
+        public string ConnectionString { get; set; }
+        public string DataBaseName { get; set; }
+    }
+}
